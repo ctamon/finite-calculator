bef3da032a432d4f113248ed152c2007895c9be7
ctamon <ctamon@clarkson.edu>
2020-02-04 10:46:17 -0500

    Added expr, term, factor.

--- main.c
+++ main.c
@@ -22,26 +22,77 @@ int main()
 	}
 }
 
+/* calculator */
+
+// handles addition
+int expr()
+{
+	int value = term();
+	while (1) {
+		if ( current_token == '+' ) {
+			match( '+' );
+			value += term();
+		}
+		else break;
+	}
+	return value;
+}
+
+// handles multiplication
+int term()
+{
+	int value = factor();
+	while (1) {
+		if ( current_token == '*' ) {
+			match( '*' );
+			value *= factor();
+		}
+		else break;
+	}
+	return value;
+}
+
+// handles brackets and numbers
+int factor()
+{
+	int value;
+
+	if ( current_token == '(' ) {
+		match( '(' );
+		value = expr();
+		match( ')' );
+		return value;
+	}
+	else if ( current_token == NUM ) {
+		value = current_attribute;
+		return value;
+	}
+	else error( "Unexpected token in factor()" );
+
+}
+
+
 /* get next token */
 int get_token()
 {
-	int c;
-	int value;
+	int c;		// current character from the stream
+	int value;	// value of a number
 
 	while (1) {
 		switch ( c = getchar() ) {
-		case '+': case '-': case '*': case '(': case ')':
+		case '+': case '*': case '(': case ')':
 			fprintf( stderr, "[OP:%c]", c );
 			return c;	// return operators and brackets as is
 		case ' ': case '\t':
 			continue;	// ignore spaces and tabs
 		default:
 			if ( isdigit(c) ) {
-				fprintf( stderr, "[%c]", c );
+				value = c - '0';
 				while ( isdigit( c = getchar() )) {
-					fprintf( stderr, "[%c]", c );
+					value = value * 10 + (c - '0');
 				}
 				ungetc( c, stdin );
+				fprintf( stderr, "[NUM:%d]", value );
 				return NUM;
 			}
 			else if ( c == '\n' ) {
@@ -55,6 +106,7 @@ int get_token()
 	}
 }
 
+// error reporting function
 void error( char *message )
 {
 	fprintf( stderr, "Error: %s\n", message );
